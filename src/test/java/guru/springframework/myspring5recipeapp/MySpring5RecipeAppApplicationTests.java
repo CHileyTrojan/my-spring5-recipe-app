ae8678397d539a64de52c58e4925ce601529162c
CHileyTrojan <chris.hiley@trojanconsultants.com>
2021-08-24 15:51:54 +0100

    Spring Initializr project setup

---
+++ src/test/java/guru/springframework/myspring5recipeapp/MySpring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.myspring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

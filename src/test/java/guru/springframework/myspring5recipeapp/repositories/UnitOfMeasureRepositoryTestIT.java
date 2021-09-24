b4ae6ad5008ac6882186e999f1a0a2424e2c13fa
CHileyTrojan <chris.hiley@trojanconsultants.com>
2021-09-24 09:32:58 +0100

    Added unit tests and Integration Test

---
+++ src/test/java/guru/springframework/myspring5recipeapp/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,37 @@
+package guru.springframework.myspring5recipeapp.repositories;
+
+import guru.springframework.myspring5recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+
+}

8ee3c4f4a9a4e63fcb713ee8333fe5bd46595423
CHileyTrojan <chris.hiley@trojanconsultants.com>
2021-10-06 11:27:55 +0100

    added display and storage of image

--- src/main/java/guru/springframework/myspring5recipeapp/services/IngredientService.java
+++ src/main/java/guru/springframework/myspring5recipeapp/services/IngredientService.java
@@ -5,4 +5,8 @@ import guru.springframework.myspring5recipeapp.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long ingredientId);
 }
---
+++ src/main/java/guru/springframework/myspring5recipeapp/services/UnitOfMeasureService.java
@@ -0,0 +1,9 @@
+package guru.springframework.myspring5recipeapp.services;
+
+import guru.springframework.myspring5recipeapp.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
